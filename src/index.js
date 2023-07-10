53f883bebbb8148af2d660398d287c7e77a8d9f7
Eugeshakw <eugeshakw@gmail.com>
2023-07-11 00:28:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
-import { ProfileCard } from 'components/profilecard/profilecard';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
