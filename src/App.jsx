cbbce893eb1058de36e82eb7b60d1239043be342
Eugeshakw <eugeshakw@gmail.com>
2023-07-11 00:53:16 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { ProfileCard } from 'components/profilecard/profilecard';
 import user from './components/datas/profileData/user.json';
 import { Statistics } from 'components/Statistics/statistics';
 import data from 'components/datas/Statistics/data.json';
+import {FriendsList} from './components/FriendsList/FriendsList';
+import friends from './components/datas/FriendsData/friends.json';
 
 export const App = () => {
   return (
@@ -14,6 +16,9 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+
+      <FriendsList friends={friends} />
+
     </>
   );
 };
--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,7 +1,20 @@
-export const FriendsList = ({ avatar, name, isOnline }) => (
+export const FriendsList = ({ friends }) => (
+  <ul>
+    {friends.map(friend => (
+      <FriendsListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+      />
+    ))}
+  </ul>
+);
+
+const FriendsListItem = ({ avatar, name, isOnline }) => (
   <li className="item">
-    <span className="status"></span>
-    <img className="avatar" src="" alt="User avatar" width="48" />
-    <p className="name"></p>
+    <span className="status">{isOnline}</span>
+    <img className="avatar" src={avatar} alt="User avatar" width="48" />
+    <p className="name">{name}</p>
   </li>
 );
